76988a911feaabf655e765386f8ea1daf7444466
angelo-bianchi <angelorabaiolibianchi@gmail.com>
2024-02-15 16:39:02 -0300

    final comments and logs added

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # sheets
+To run the code, you need to:
+
+Have Python 3 or higher installed
+
+Install the libraries:
+pip install --upgrade google-api-python-client google-auth-httplib2 google-auth-oauthlib
+
+Create a JSON file named token.json with the following information:
+{"token": "ya29.a0AfB_byDoEJ6cJRhqKxp-5BwtiDQFaiM4C5b_uxN6O-stjpoiKqRINT0GK2E-qRQULx96P37bjDy71HqicR2veUlXm1WSuKRoioM1TBN0cCU8tSpioJhLeejyVXIA86s2qsVRbJ2BJSuJtvya0O2rc6Pgd3HK8ZLIZsMGaCgYKAZcSARMSFQHGX2Mii1ST4mHfBWGDvJduJGgoIQ0171", "refresh_token": "1//0h7HdwT95bvrlCgYIARAAGBESNwF-L9IrfvzV7prNRCG5AHhd4oOUHwal5y3OtN91Bkt21m3CnZ4Kc1r8TLD9st6nRne1iiZd2M4", "token_uri": "https://oauth2.googleapis.com/token", "client_id": "444548271737-1tg19aj8uroinkmeg92pvasbl2eeusp9.apps.googleusercontent.com", "client_secret": "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", "scopes": ["https://www.googleapis.com/auth/spreadsheets"], "universe_domain": "googleapis.com", "account": "", "expiry": "2024-02-15T19:33:30.131548Z"}
--- sheets.py
+++ sheets.py
@@ -6,18 +6,21 @@ from google_auth_oauthlib.flow import InstalledAppFlow
 from googleapiclient.discovery import build
 from googleapiclient.errors import HttpError
 
+#Permissions within the API
 SCOPES = ["https://www.googleapis.com/auth/spreadsheets"]
 
-# The ID and range of a sample spreadsheet.
+# The ID and range of the spreadsheet
 SAMPLE_SPREADSHEET_ID = "1tXHai4DDZ2SdIEXnwrYwFMaot_MmIakQK01q0i31lV0"
 SAMPLE_RANGE_NAME = "engenharia_de_software!A4:H"
 
-
+# Checking credentials and validating credentials
 def main():
   creds = None
+  #Check if file token.json exists
   if os.path.exists("token.json"):
     creds = Credentials.from_authorized_user_file("token.json", SCOPES)
   if not creds or not creds.valid:
+#Check the credentials.json file to valid acess
     if creds and creds.expired and creds.refresh_token:
       creds.refresh(Request())
     else:
@@ -25,14 +28,12 @@ def main():
           "credentials.json", SCOPES
       )
       creds = flow.run_local_server(port=0)
-
+# Creates file token.json with the file credentials.json to access the spreadsheet
     with open("token.json", "w") as token:
       token.write(creds.to_json())
-
+#Read the spreadsheets
   try:
     service = build("sheets", "v4", credentials=creds)
-
-
     sheet = service.spreadsheets()
     result = (
         sheet.values()
@@ -40,18 +41,20 @@ def main():
         .execute()
     )
     values = result.get("values", [])
-
+#Check if spreadsheet is empty
     if not values:
       print("No data found.")
       return
-
+#Caculations for Faltas and Notas
     for row in values:
+      line = str(row[0])
+      name = str(row[1])
       falta = int(row[2])
       grade1 = float(row[3])
       grade2 = float(row[4])
       grade3 = float(row[5])
       media = round((grade1+grade2+grade3)/3)
-      
+      #Check the status of Sitação and Nota para Aprovação Final
       if falta >15:
         situacao = "Reprovado por falta"
         naf = 0
@@ -59,23 +62,22 @@ def main():
         situacao ="Reprovado por nota"
         naf = 0
       elif media >=70:
-        situacao = "Aprovado"
+        situacao = "Aprovado por nota"
         naf = 0
       elif media>=50 and media<70:
         naf = (100-media)
-        situacao = "Exame"
+        situacao = "Exame final"
       linha = int(row[0])+3
+      #Update the spreadsheet with the new cell values for Situação and Nota para Aprovação Final
       update_values('G'+str(linha),'USER_ENTERED',situacao)
-      update_values('H'+str(linha),'USER_ENTERED',naf)    
-
+      update_values('H'+str(linha),'USER_ENTERED',naf)
+      print(line + " - " + name +" - "+situacao+" - "+str(naf))
   except HttpError as err:
     print(err)
 
-
-
-
-
+#Update cells function
 def update_values(range_name, value_input_option, _values):
+  #Check credentials
   creds = Credentials.from_authorized_user_file("token.json", SCOPES)
   try:
     service = build("sheets", "v4", credentials=creds)
@@ -83,7 +85,7 @@ def update_values(range_name, value_input_option, _values):
         [
             _values
         ],
-
+#Define input spreadsheet,range and type
     ]
     body = {"values": values}
     result = (
@@ -97,7 +99,8 @@ def update_values(range_name, value_input_option, _values):
           )
         .execute()
     )
-    print(f"{result.get('updatedCells')} cells updated.")
+
+    #Print cells values updated with name, Situação + Nota para Aprovação Final
     return result
   except HttpError as error:
     print(f"An error occurred: {error}")
@@ -105,6 +108,6 @@ def update_values(range_name, value_input_option, _values):
 
 
 
-
+#Runs code
 if __name__ == "__main__":
   main()
